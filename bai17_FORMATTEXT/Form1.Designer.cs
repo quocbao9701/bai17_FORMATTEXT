c3cb3f6adcec8736bc7e172731ba7a7e054b78d0
SPC <SPC@05-LAB402>
2025-03-28 08:51:39 +0700

    Add project files.

---
+++ bai17_FORMATTEXT/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace bai17_FORMATTEXT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.radRed = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.txtnhapten = new System.Windows.Forms.TextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblLaptrinh = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chkBold = new System.Windows.Forms.CheckBox();
+            this.chkitalic = new System.Windows.Forms.CheckBox();
+            this.chkgachchan = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(164, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NHẬP TÊN";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(49, 93);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 212);
+            this.listBox1.TabIndex = 1;
+            // 
+            // radRed
+            // 
+            this.radRed.AutoSize = true;
+            this.radRed.ForeColor = System.Drawing.Color.Red;
+            this.radRed.Location = new System.Drawing.Point(60, 122);
+            this.radRed.Name = "radRed";
+            this.radRed.Size = new System.Drawing.Size(48, 17);
+            this.radRed.TabIndex = 2;
+            this.radRed.TabStop = true;
+            this.radRed.Text = "RED";
+            this.radRed.UseVisualStyleBackColor = true;
+            this.radRed.CheckedChanged += new System.EventHandler(this.radRed_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.ForeColor = System.Drawing.Color.Lime;
+            this.radioButton2.Location = new System.Drawing.Point(60, 155);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(63, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "GREEN";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.ForeColor = System.Drawing.Color.Blue;
+            this.radioButton3.Location = new System.Drawing.Point(60, 194);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(53, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "BLUE";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.ForeColor = System.Drawing.Color.Black;
+            this.radioButton4.Location = new System.Drawing.Point(60, 236);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(59, 17);
+            this.radioButton4.TabIndex = 2;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "BLACK";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // txtnhapten
+            // 
+            this.txtnhapten.Location = new System.Drawing.Point(260, 6);
+            this.txtnhapten.Name = "txtnhapten";
+            this.txtnhapten.Size = new System.Drawing.Size(203, 20);
+            this.txtnhapten.TabIndex = 3;
+            this.txtnhapten.Text = "TÈO 2011";
+            this.txtnhapten.TextChanged += new System.EventHandler(this.txtnhapten_TextChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(343, 93);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 212);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(49, 93);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 212);
+            this.listBox3.TabIndex = 1;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(343, 93);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(151, 212);
+            this.listBox4.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(48, 350);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "LẬP TRÌNH BỞI";
+            // 
+            // lblLaptrinh
+            // 
+            this.lblLaptrinh.Location = new System.Drawing.Point(176, 350);
+            this.lblLaptrinh.Name = "lblLaptrinh";
+            this.lblLaptrinh.Size = new System.Drawing.Size(203, 20);
+            this.lblLaptrinh.TabIndex = 3;
+            this.lblLaptrinh.Text = "TÈO 2011";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(415, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 55);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "THOÁT";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // chkBold
+            // 
+            this.chkBold.AutoSize = true;
+            this.chkBold.ForeColor = System.Drawing.Color.Blue;
+            this.chkBold.Location = new System.Drawing.Point(359, 137);
+            this.chkBold.Name = "chkBold";
+            this.chkBold.Size = new System.Drawing.Size(82, 17);
+            this.chkBold.TabIndex = 5;
+            this.chkBold.Text = "ĐẬM BOLD";
+            this.chkBold.UseVisualStyleBackColor = true;
+            this.chkBold.CheckedChanged += new System.EventHandler(this.chkBold_CheckedChanged);
+            // 
+            // chkitalic
+            // 
+            this.chkitalic.AutoSize = true;
+            this.chkitalic.ForeColor = System.Drawing.Color.Blue;
+            this.chkitalic.Location = new System.Drawing.Point(359, 178);
+            this.chkitalic.Name = "chkitalic";
+            this.chkitalic.Size = new System.Drawing.Size(112, 17);
+            this.chkitalic.TabIndex = 5;
+            this.chkitalic.Text = "NGHIÊNG ITALIC";
+            this.chkitalic.UseVisualStyleBackColor = true;
+            this.chkitalic.CheckedChanged += new System.EventHandler(this.chkitalic_CheckedChanged);
+            // 
+            // chkgachchan
+            // 
+            this.chkgachchan.AutoSize = true;
+            this.chkgachchan.ForeColor = System.Drawing.Color.Blue;
+            this.chkgachchan.Location = new System.Drawing.Point(359, 216);
+            this.chkgachchan.Name = "chkgachchan";
+            this.chkgachchan.Size = new System.Drawing.Size(89, 17);
+            this.chkgachchan.TabIndex = 5;
+            this.chkgachchan.Text = "GẠCH CHÂN";
+            this.chkgachchan.UseVisualStyleBackColor = true;
+            this.chkgachchan.CheckedChanged += new System.EventHandler(this.chkgachchan_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 536);
+            this.Controls.Add(this.chkgachchan);
+            this.Controls.Add(this.chkitalic);
+            this.Controls.Add(this.chkBold);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblLaptrinh);
+            this.Controls.Add(this.txtnhapten);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radRed);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RadioButton radRed;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.TextBox txtnhapten;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox lblLaptrinh;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox chkBold;
+        private System.Windows.Forms.CheckBox chkitalic;
+        private System.Windows.Forms.CheckBox chkgachchan;
+    }
+}
+
---
+++ bai17_FORMATTEXT/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai17_FORMATTEXT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    
+
+        private void txtnhapten_TextChanged(object sender, EventArgs e)
+        {
+            lblLaptrinh.Text = txtnhapten.Text;
+        }
+
+        private void radRed_CheckedChanged(object sender, EventArgs e)
+        {
+            lblLaptrinh.ForeColor = Color.Red;
+            txtnhapten.ForeColor = Color.Red;   
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            lblLaptrinh.ForeColor = Color.Green;
+            txtnhapten.ForeColor= Color.Green;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            lblLaptrinh.ForeColor = Color.Blue;
+            txtnhapten.ForeColor=Color.Blue;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            lblLaptrinh.ForeColor = Color.Black;
+            txtnhapten.ForeColor = Color.Black;
+        }
+
+        private void chkBold_CheckedChanged(object sender, EventArgs e)
+        {
+            lblLaptrinh.Font = new Font(lblLaptrinh.Font.Name,lblLaptrinh.Font.Size,FontStyle.Bold);
+
+        }
+
+        private void chkitalic_CheckedChanged(object sender, EventArgs e)
+        {
+            lblLaptrinh.Font = new Font(lblLaptrinh.Font.Name, lblLaptrinh.Font.Size, FontStyle.Italic);
+        }
+
+        private void chkgachchan_CheckedChanged(object sender, EventArgs e)
+        {
+            lblLaptrinh.Font = new Font(lblLaptrinh.Font.Name,lblLaptrinh.Font.Size,FontStyle.Underline);
+        }
+    }
+}
